f77c43b0db85db878cc82420da70d8c33d7a735c
Christie Bahlai <cbahlai@kent.edu>
2021-09-30 10:04:25 -0400

    bring in weather data, rough out gap handling function

--- Main_analysis.R
+++ Main_analysis.R
@@ -6,7 +6,7 @@
 LB<-read.csv(file="data/KBS_Haxy_C7_1989-2020.csv", header=T,
              na.strings=c(NA))
 
-
+#### some silly functions, conditionals and loops to demonstrate their structure in R
 sum.of.squares <- function(x,y) {#paste in demo function that calculates sum of squares
   x^2 + y^2 #add square of x to square of y
 }
@@ -26,39 +26,34 @@ iszero("cherry pie")#demonstrate iszero function
 vec<-c(1,2,3,0)#define a test vector for demonstrating a function on a loop
 
 for(i in 1:length(vec)){#demonstrate a simple loop 
-  iszero(vec[i])
+  iszero(vec[i])#is each item in the vector zero?
 }
-
+##### enough of all that, what about real data?
 summary(LB)
 
 #clean data
-#fix dates, make them ISO'ed
+#first, we fix dates, make them ISO'ed
 library(lubridate)
+
 LB$newdate<-mdy(LB$DATE)#parses the date format used for the forest plots
 LB$newdate<-mdy_hm(LB$DATE)#parses the date format used in the main plots
 
+#well, crap, neither command gets all of the dates to parse correctly
+#we have an issue because data from the main site exported with time stamps but 
+#the forest site did not. Ugh.
 
+
+#Christie's solution: brute force removal of timestamps
+LB$DATE<-gsub(" 0:00", "", LB$DATE)#remove time stamp strings
+
+LB$newdate<-mdy(LB$DATE)#parses the date format now used by all observations
+
+summary(LB)#bingo! looks like it worked!
 
 ###################################
 #UNADAPTED PASTED CODE BELOW WILL NOT WORK
 
-#extract year
-lampyrid$year<-year(lampyrid$newdate)
-#extract day of year. DOY is very useful for a phenology type analyses
-#because you don't have to deal with day-of-month numbers starting over 
-#in the middle of a phenological event.
-lampyrid$DOY<-yday(lampyrid$newdate)
-#use ISO week, so we start counting on Monday, not Jan 1, COOL! Our sampling usually 
-#takes place Wed-Friday, so if we use week of year stating on Jan 1, there is a good chance that
-#samples taken within a sampling week would get grouped incorrectly when we go to do the analysis.
-lampyrid$week<-isoweek(lampyrid$newdate)
 
-#let's look for the data problems we found we used OpenRefine and see if
-#we can impliment our cleaning operations here- that way we have a complete
-#record of EVERYTHING that happened to these data. Recall there were issues 
-#with TREAT_DESC
-#let's look at these columns individually and fix errors as we find them
-#and we should also check for weirdness in our numeric values
 
 #download weather data from KBS weather station
 weather<-read.table(file="http://lter.kbs.msu.edu/datatables/7.csv",
@@ -72,20 +67,17 @@ weather<-read.table(file="http://lter.kbs.msu.edu/datatables/7.csv",
 weather$DOY<-yday(weather$date)
 weather$week<-isoweek(weather$date)
 #do a few simple plots to make sure the data makes sense -this is
-#a good way to check that the importation was sucessful
+#a good way to check that the importation was successful
 
 plot(weather$DOY, weather$air_temp_mean)
 plot(weather$DOY, weather$precipitation)
 
-#because we don't have lampyrid records before 2004, let's cut out the data
-#from before 2003 so we can process the weaqther data more quickly. Also our
-#lampyrid data stops at the end of 2015 and for some reason the new
-#weather station data breaks our code. DANGIT. so we'll cut off the weather
+#let's cut out the data from before 1989 so we can process the weather data more quickly. Al so we'll cut off the weather
 #data that's causing us problems- we don't need it anyway
-weather<-subset(weather, weather$year>2003& weather$year<2016)
+weather<-subset(weather, weather$year>=1989& weather$year<=2020)
 
 
-#lets also get rid of the vairables we don't need:
+#lets also get rid of the variables we don't need:
 weather$flag_precip<-NULL
 weather$flag_air_temp_mean<-NULL
 weather$flag_air_temp_max<-NULL
@@ -96,9 +88,34 @@ weather$flag_air_temp_min<-NULL
 
 weather<-weather[order(weather$year, weather$DOY),]
 
+#Let's examine the data to see how complete it is
+summary(weather)
+
 #lets's pre-process these weather data so we get rid of missing values
 # we can write a function to do this for us.
 #if missing data is rare, it is probably safe to assume that missing
 #temperatures are similar to the weather on the day before or after.
 #for the sake of simplicity, let's replace a missing value with the 
 #value for that variable for the day before
+
+#first, define the function
+
+replace.missing<-function(vec){
+  #create a vector to put our new values into
+  New = c()
+  for (i in 1:(length(vec))){
+    if (is.na(vec[i])){
+      vec[i]<-vec[i-1]
+      #if the data is missing, sub in the value from the measurement before
+      
+    } else{
+      #if the value is not missing, just pass it through to the result vector
+      vec[i]<-vec[i]
+    }
+    New=c(New, vec[i])
+  }
+  if (any(is.na(New))){
+    replace.missing(New)
+  }
+  return(New)
+}
